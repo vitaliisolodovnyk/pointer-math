a283322ccbabcc357567520fcd1a15fc5c4562c4
Vitalii Solodovnyk <vitaliysecretua@gmail.com>
2020-01-16 13:36:13 +0200

    add methods

---
+++ src/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MyMath.addition(5, 6, 7));
+        System.out.println(MyMath.subtraction(5, 6, 7));
+        System.out.println(MyMath.multiplication(5, 6, 7));
+    }
+}
---
+++ src/src/MyMath.java
@@ -0,0 +1,17 @@
+public class MyMath {
+    public static final int x = 5;
+    public static final int y = 6;
+    public static final int z = 7;
+
+    public static int addition(int x, int y, int z){
+        return x+y+z;
+    }
+
+    public static int subtraction(int x, int y, int z) {
+        return x-y-z;
+    }
+
+    public static int multiplication(int x, int y, int z) {
+        return x*y*z;
+    }
+}
